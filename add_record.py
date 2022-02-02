acfc855a77d63bb069ef74c606ba1a872289459b
Josiah <duhaimejosiah@gmail.com>
2022-02-02 11:33:34 -0800

    new logic for faster add of record

--- add_record.py
+++ add_record.py
@@ -1,4 +1,6 @@
 # imports
+import datetime as dt
+
 import psycopg2
 
 # db global variables
@@ -21,6 +23,8 @@ class CreateRecord:
 
     def __init__(self):
         self.company = self.get_company()
+
+    def get_details(self):
         self.status = self.get_status()
         self.date_applied = self.get_date_applied()
         self.last_contact = self.get_last_contact()
@@ -35,26 +39,42 @@ class CreateRecord:
         return input(f'date applied (YYYY-MM-DD):\n> ')
 
     def get_last_contact(self):
-        input_ = input(f'date of last contact (YYYY-MM-DD):\n> ')
-        if input_ != '':
-            return input
+        return input(f'date of last contact (YYYY-MM-DD):\n> ')
 
 
 if __name__ == '__main__':
     print('Please provide inputs.\n')
 
+    SIMPLE_INPUTS = True
+
+    version_check = input(
+        'enter any character for the simple (fast) input:\n> ')
+
+    if not version_check:
+        SIMPLE_INPUTS = False
+
     # get data
     record = CreateRecord()
 
-    # build query
-    query = f"""
-            INSERT INTO jobs (
-                company, status, date_applied
-            )
-            VALUES (
-                '{record.company}', '{record.status}', '{record.date_applied}'
-            );
-            """
+    if SIMPLE_INPUTS:
+        query = f"""
+                INSERT INTO jobs (
+                    company, status, date_applied
+                )
+                VALUES (
+                    '{record.company}', 'submitted-application', '{dt.date.today()}'
+                );
+                """
+    else:
+        record.get_details()
+        query = f"""
+                INSERT INTO jobs (
+                    company, status, date_applied
+                )
+                VALUES (
+                    '{record.company}', '{record.status}', '{record.date_applied}'
+                );
+                """
 
     # push data to the database
     with conn.cursor() as c:
